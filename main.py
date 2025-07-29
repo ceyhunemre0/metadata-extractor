dd09677aab5c032da4edc7199419c72c5377f6c6
ceyhunemre0 <ceyhunemretop0@gmail.com>
2025-07-29 10:13:03 +0300

    content okuma eklendi.

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ raw = parser.from_file('data/1.pdf')
 # Metadata kısmını al
 metadata = raw['metadata']
 
-with open("logs/metadata.log", "w", encoding="utf-8") as log_file:
-    for key, value in metadata.items():
-        log_file.write(f"{key}: {value}\n")
+def extract_metadata(metadata):
+    # Metadata'dan gerekli bilgileri al
+    with open("logs/metadata.log", "w", encoding="utf-8") as log_file:
+        for key, value in metadata.items():
+            log_file.write(f"{key}: {value}\n")
+
+
+content = raw['content']
+def extract_content(content):
+    # İçeriği log dosyasına yaz
+    with open("logs/content.log", "w", encoding="utf-8") as log_file:
+        log_file.write(content)
+
+extract_content(content)
